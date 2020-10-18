8707de6d6d4c9f5300aea9eb7068539a870827a8
LAPTOP-6U7FNM08\nibu_ <nibueapen@gmail.com>
2020-10-18 10:04:56 -0500

    Tray notification

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,6 +23,7 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Button1 = New System.Windows.Forms.Button()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
@@ -32,13 +33,15 @@ Partial Class Form1
         Me.TextBox3 = New System.Windows.Forms.TextBox()
         Me.TextBox4 = New System.Windows.Forms.TextBox()
         Me.scrnsvr = New System.Windows.Forms.Timer(Me.components)
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(157, 238)
+        Me.Button1.Location = New System.Drawing.Point(209, 293)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(109, 30)
+        Me.Button1.Size = New System.Drawing.Size(145, 37)
         Me.Button1.TabIndex = 4
         Me.Button1.Text = "Login"
         Me.Button1.UseVisualStyleBackColor = True
@@ -46,36 +49,40 @@ Partial Class Form1
         'TextBox1
         '
         Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
-        Me.TextBox1.Location = New System.Drawing.Point(47, 66)
+        Me.TextBox1.Location = New System.Drawing.Point(63, 81)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(218, 13)
+        Me.TextBox1.Size = New System.Drawing.Size(291, 15)
         Me.TextBox1.TabIndex = 1
         '
         'TextBox2
         '
         Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None
         Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox2.Location = New System.Drawing.Point(47, 145)
+        Me.TextBox2.Location = New System.Drawing.Point(63, 178)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(4)
         Me.TextBox2.Name = "TextBox2"
         Me.TextBox2.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
-        Me.TextBox2.Size = New System.Drawing.Size(218, 13)
+        Me.TextBox2.Size = New System.Drawing.Size(291, 16)
         Me.TextBox2.TabIndex = 2
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(39, 41)
+        Me.Label1.Location = New System.Drawing.Point(52, 50)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.Size = New System.Drawing.Size(73, 17)
         Me.Label1.TabIndex = 3
         Me.Label1.Text = "Username"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(39, 117)
+        Me.Label2.Location = New System.Drawing.Point(52, 144)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.Size = New System.Drawing.Size(69, 17)
         Me.Label2.TabIndex = 4
         Me.Label2.Text = "Password"
         '
@@ -84,9 +91,10 @@ Partial Class Form1
         Me.CheckBox1.AutoSize = True
         Me.CheckBox1.Checked = True
         Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
-        Me.CheckBox1.Location = New System.Drawing.Point(43, 196)
+        Me.CheckBox1.Location = New System.Drawing.Point(57, 241)
+        Me.CheckBox1.Margin = New System.Windows.Forms.Padding(4)
         Me.CheckBox1.Name = "CheckBox1"
-        Me.CheckBox1.Size = New System.Drawing.Size(135, 17)
+        Me.CheckBox1.Size = New System.Drawing.Size(178, 21)
         Me.CheckBox1.TabIndex = 3
         Me.CheckBox1.Text = "Remember login details"
         Me.CheckBox1.UseVisualStyleBackColor = True
@@ -96,10 +104,11 @@ Partial Class Form1
         Me.TextBox3.BackColor = System.Drawing.SystemColors.Window
         Me.TextBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.TextBox3.Enabled = False
-        Me.TextBox3.Location = New System.Drawing.Point(40, 135)
+        Me.TextBox3.Location = New System.Drawing.Point(53, 166)
+        Me.TextBox3.Margin = New System.Windows.Forms.Padding(4)
         Me.TextBox3.Multiline = True
         Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(227, 35)
+        Me.TextBox3.Size = New System.Drawing.Size(302, 43)
         Me.TextBox3.TabIndex = 6
         '
         'TextBox4
@@ -107,22 +116,28 @@ Partial Class Form1
         Me.TextBox4.BackColor = System.Drawing.SystemColors.Window
         Me.TextBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.TextBox4.Enabled = False
-        Me.TextBox4.Location = New System.Drawing.Point(41, 58)
+        Me.TextBox4.Location = New System.Drawing.Point(55, 71)
+        Me.TextBox4.Margin = New System.Windows.Forms.Padding(4)
         Me.TextBox4.Multiline = True
         Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(227, 35)
+        Me.TextBox4.Size = New System.Drawing.Size(302, 43)
         Me.TextBox4.TabIndex = 7
         '
         'scrnsvr
         '
-        Me.scrnsvr.Enabled = True
         Me.scrnsvr.Interval = 5000
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "NotifyIcon1"
+        Me.NotifyIcon1.Visible = True
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(898, 471)
+        Me.ClientSize = New System.Drawing.Size(1197, 580)
         Me.Controls.Add(Me.CheckBox1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
@@ -131,9 +146,10 @@ Partial Class Form1
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.TextBox3)
         Me.Controls.Add(Me.TextBox4)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.MaximizeBox = False
         Me.Name = "Form1"
-        Me.Text = "E Office"
+        Me.Text = "Chat App"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -148,4 +164,5 @@ Partial Class Form1
     Friend WithEvents TextBox3 As TextBox
     Friend WithEvents TextBox4 As TextBox
     Friend WithEvents scrnsvr As Timer
+    Friend WithEvents NotifyIcon1 As NotifyIcon
 End Class
--- Form1.vb
+++ Form1.vb
@@ -14,4 +14,38 @@
 
     End Sub
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        NotifyIcon1.BalloonTipIcon = ToolTipIcon.Info
+        NotifyIcon1.BalloonTipText = "Welcome to TutorialsPanel.com!!"
+        NotifyIcon1.BalloonTipTitle = "Welcome Message"
+        NotifyIcon1.ShowBalloonTip(2000)
+    End Sub
+
+    Private Sub Form1_Resize(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Resize
+        If Me.WindowState = FormWindowState.Minimized Then
+            NotifyIcon1.Visible = True
+            'NotifyIcon1.Icon = SystemIcons.Application
+            NotifyIcon1.BalloonTipIcon = ToolTipIcon.Info
+            NotifyIcon1.BalloonTipTitle = "Verificador corriendo"
+            NotifyIcon1.BalloonTipText = "Verificador corriendo"
+            NotifyIcon1.ShowBalloonTip(50000)
+            'Me.Hide()
+            ShowInTaskbar = False
+        End If
+    End Sub
+
+
+    Private Sub NotifyIcon1_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles NotifyIcon1.DoubleClick
+        'Me.Show()
+        ShowInTaskbar = True
+        Me.WindowState = FormWindowState.Normal
+        NotifyIcon1.Visible = False
+    End Sub
+
+    Private Sub NotifyIcon1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon1.MouseDoubleClick
+
+    End Sub
 End Class
+
+
+
