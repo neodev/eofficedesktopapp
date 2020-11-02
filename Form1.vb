536c82ade35bdc789bbdd613a63a2ab5974e8bec
LAPTOP-6U7FNM08\nibu_ <nibueapen@gmail.com>
2020-11-02 15:49:24 -0600

    function moved

--- Form1.vb
+++ Form1.vb
@@ -605,57 +605,7 @@ Public Class Form1
 
     End Function
 
-    Public Function GetAuthKey() As String
 
-        authkey.Text = ""
-
-        Dim currnettime As String = Now.ToString(mysqldateformat)
-
-        If (TextBox1.Text <> "" And TextBox2.Text <> "") Then
-
-            Dim postData As String = "un=" & TextBox1.Text & "&pw=" & TextBox2.Text & "&ct=" & currnettime
-            Dim tempCookies As New CookieContainer
-            Dim encoding As New UTF8Encoding
-            Dim byteData As Byte() = encoding.GetBytes(postData)
-
-            Dim postReq As HttpWebRequest = DirectCast(WebRequest.Create(apiurl), HttpWebRequest)
-            postReq.Method = "POST"
-            postReq.KeepAlive = True
-            postReq.CookieContainer = tempCookies
-            postReq.ContentType = "application/x-www-form-urlencoded"
-            postReq.Referer = apiurl
-            postReq.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0"
-            postReq.ContentLength = byteData.Length
-
-            Dim postreqstream As Stream = postReq.GetRequestStream()
-            postreqstream.Write(byteData, 0, byteData.Length)
-            postreqstream.Close()
-
-            Dim postresponse As HttpWebResponse
-            postresponse = DirectCast(postReq.GetResponse(), HttpWebResponse)
-            tempCookies.Add(postresponse.Cookies)
-            logincookie = tempCookies
-
-            Dim postreqreader As New StreamReader(postresponse.GetResponseStream())
-            Dim thepage As String = postreqreader.ReadToEnd
-
-            RichTextBox1.Text = thepage
-
-            Dim jss As New JavaScriptSerializer()
-            Dim datadict As Dictionary(Of String, String) = jss.Deserialize(Of Dictionary(Of String, String))(thepage)
-
-            authkey.Text = datadict("uid")
-
-
-        End If
-
-        If authkey.Text <> "" Then
-            Return True
-        Else
-            Return False
-        End If
-
-    End Function
 
     Public Function Login() As Boolean
 
